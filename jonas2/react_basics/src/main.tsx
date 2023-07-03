0f797ab2ecde7a850781cd1e9e0aa36251cd39fe
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-07-03 15:58:12 +0500

    .

--- jonas2/react_basics/src/main.tsx
+++ jonas2/react_basics/src/main.tsx
@@ -3,9 +3,7 @@ import { Root, createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const element: HTMLDivElement = document.getElementById(
-  'root'
-) as HTMLDivElement;
+const element: HTMLDivElement = document.getElementById('root') as HTMLDivElement;
 
 const root: Root = createRoot(element);
 
