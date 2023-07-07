2931d800d9f590ae947d3e0f381d35808db329ea
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-07-07 16:25:05 +0500

    .

--- jonas2/far-away/src/components/step-numbers/step-numbers.tsx
+++ jonas2/far-away/src/components/step-numbers/step-numbers.tsx
@@ -4,9 +4,9 @@ type StepNumbersType = {
 function StepNumbers({ step }: StepNumbersType) {
   return (
     <div className="numbers">
-      <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-      <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-      <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      <div className={`btn ${step >= 1 ? 'active' : ''}`}>1</div>
+      <div className={`btn ${step >= 2 ? 'active' : ''}`}>2</div>
+      <div className={`btn ${step >= 3 ? 'active' : ''}`}>3</div>
     </div>
   );
 }
