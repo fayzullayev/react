8fc0d3820b809a133090fb7468f53f8c8f640de5
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-07-13 10:25:33 +0500

    Update main.tsx

--- jonas2/eat_n_split/src/main.tsx
+++ jonas2/eat_n_split/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = document.getElementById('root') as HTMLDivElement;
+
+const root = createRoot(element);
+
+root.render(<App />);
