af322335d0bf0e5a657b1e1af281591dbd4e56be
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-07-14 17:20:35 +0500

    .

---
+++ jonas2/usePopCorn/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const element = document.getElementById('root') as HTMLElement;
+
+const root = ReactDOM.createRoot(element);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
