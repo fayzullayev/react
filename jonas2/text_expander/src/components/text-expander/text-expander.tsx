4faf3d159c307483ce05f3409e5436515e542d64
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-08-03 11:24:30 +0500

    Update text-expander.tsx

--- jonas2/text_expander/src/components/text-expander/text-expander.tsx
+++ jonas2/text_expander/src/components/text-expander/text-expander.tsx
@@ -1,44 +1,45 @@
-import {useState} from "react";
+import { useState } from "react";
 
 type TextExpanderProps = {
-    expanded?: boolean;
-    className?: string;
-    collapsedNumWords?: number;
-    expandButtonText?: string;
-    collapseButtonText?: string;
-    buttonColor?: string;
-    children: string;
+  expanded?: boolean;
+  className?: string;
+  collapsedNumWords?: number;
+  expandButtonText?: string;
+  collapseButtonText?: string;
+  buttonColor?: string;
+  children: string;
 };
 
 function TextExpander({
-                          expandButtonText = "Show more",
-                          collapseButtonText = "Show less",
-                          buttonColor = "blue",
-                          expanded = false,
-                          collapsedNumWords = 10,
-                          className = "",
-                          children,
-                      }: TextExpanderProps) {
-    const [isExpanded, setIsExpanded] = useState<boolean>(expanded);
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "blue",
+  expanded = false,
+  collapsedNumWords = 10,
+  className = "",
+  children,
+}: TextExpanderProps) {
+  const [isExpanded, setIsExpanded] = useState<boolean>(expanded);
 
-    let words;
+  let words;
 
-    if (!isExpanded) {
-        const wordsArray = children?.split(" ");
-        words = wordsArray?.slice(0, collapsedNumWords);
-        words = words.join(" ");
-        words += "...";
-    } else {
-        words = children;
-    }
+  if (!isExpanded) {
+    const wordsArray = children?.split(" ");
+    words = wordsArray?.slice(0, collapsedNumWords);
 
-    return (
-        <div>
-            {words}
-            {isExpanded ?}
-            <hr/>
-        </div>
-    );
+    words = words.join(" ");
+    words += "...";
+  } else {
+    words = children;
+  }
+
+  return (
+    <div>
+      {words}
+      {/*{isExpanded ?}*/}
+      <hr />
+    </div>
+  );
 }
 
 export default TextExpander;
