a49d94c8759f9ddee1bb0e725d1316d0a1f897c2
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-12-25 11:48:05 +0500

    js refresher

--- max-new-journey/js-refresh/main.js
+++ max-new-journey/js-refresh/main.js
@@ -1,5 +1,66 @@
-import def, { apiKey, ggg } from '/util.js';
+// function greet(firstName, lastName = 'Nabiyeva', ...args) {
+//   console.log(firstName, lastName, 'args:', args.length);
+// }
+// const greet = (firstName, lastName = 'Nabiyeva', ...args) => {
+//   console.log(firstName, lastName, 'args:', args.length);
+// };
+//
+// greet('Mirodil', 'Fayzullayev', 1, 2, 3);
+// greet('Iroda');
+//
+// console.log('Hello');
 
-console.log('default', def);
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   greet() {
+//     console.log('Hi!');
+//   }
+// }
+//
+// const user1 = new User();
+// user1.greet();
+// const user = {
+//   name: 'Mirodil',
+//   age: 29,
+//   greet(firstName, lastName = 'Nabiyeva', ...args) {
+//     console.log(firstName, lastName, 'args:', args.length, this);
+//   },
+//   greet1: function () {
+//     console.log(this);
+//   },
+// };
+//
+// console.log(user.name);
+// console.log(user['age']);
+// // user['greet'](1, 2, 3);
+// user['greet1'](1, 2, 3);
 
-console.log('named export', apiKey, ggg);
+const hobbies = ['Sports', 'Cooking', 'Reading'];
+
+const [hobby1, hobby2, hobby3] = hobbies;
+
+console.log(hobby1, hobby2, hobby3);
+//
+// console.log(hobbies);
+// // console.log(hobbies[0]);
+//
+// hobbies.push('Working');
+// console.log(hobbies);
+//
+// const index = hobbies.findIndex((item) => item === 'Cooking');
+//
+// console.log(hobbies.map((item) => ({ text: item })));
+
+const user = {
+  // name: 'Max',
+  // age: 34,
+};
+
+function log({ name = 'Mirodil', age: yosh = 26 }) {
+  console.log(name, yosh);
+}
+
+log(user);
--- max-new-journey/js-refresh/util.js
+++ max-new-journey/js-refresh/util.js
@@ -1,5 +1,4 @@
-let apiKey = '2oen93u4nf843hfbn9u4i3onfio23nfiu23i23fn';
+export let apiKey = '2oen93u4nf843hfbn9u4i3onfio23nfiu23i23fn';
 export const ggg = 'qwertyuiop';
 
-export { apiKey };
 export default { jjj: 0 };
