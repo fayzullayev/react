9510c06bf98b136d159b3a561c5f257590b8d655
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-03-26 14:32:30 +0500

    setup

--- max-new-journey/essentials/src/main.tsx
+++ max-new-journey/essentials/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')!;
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
