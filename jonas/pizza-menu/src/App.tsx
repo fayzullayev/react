8d91918a857aea9d460f25c9efefad9647815741
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-08-05 21:29:22 +0500

    practise

---
+++ jonas/pizza-menu/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const isAdmin = false;
+
+  console.log(isAdmin && undefined && 56 && null && 'This book has a movie');
+
+  return <h1></h1>;
+}
+
+export default App;
---
+++ jonas/pizza-menu/src/main.tsx
@@ -0,0 +1,29 @@
+// import ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
+//
+// ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+
+// let userProfile = {
+//   preferences: {
+//     theme: 'dark',
+//   },
+// };
+//
+// let theme = userProfile.preferences.theme ?? 'light';
+//
+// console.log(theme);
+
+// const value = ;
+//
+// console.log(value && 'World');
+
+var a = 1;
+
+function b() {
+  a = 10;
+  return;
+  function a() {}
+}
+b();
+console.log(a);
